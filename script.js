0e5f76525c9d87726cbcb7c655c5f56ad0f1d4ff
igor <igor1_souza@hotmail.com>
2023-09-21 15:12:29 -0400

    Prototipo 1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger').addEventListener('click', function() {
+    const hamburgerNav = document.querySelector('.hamburger-nav');
+    hamburgerNav.classList.toggle('active');
+});
